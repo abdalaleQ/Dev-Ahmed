c2a91b2b802ff821340f3ad3cdce0b688a589918
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2024-02-20 19:57:39 +0300

    Update ping.py

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -59,8 +59,8 @@ async def ping_fallen(_, message: Message):
                 [
                     InlineKeyboardButton("الدعم", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
-                        "السوࢪس",
-                        url="https://t.me/T7_AU",
+                        "سورس الزعيم",
+                        url="https://t.me/VVV5P",
                     ),
                 ],
             ]
